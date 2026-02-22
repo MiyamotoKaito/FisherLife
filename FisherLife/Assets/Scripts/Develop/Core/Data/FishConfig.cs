1debc400cd33e7a0ec1e4d1b7cb2c7e9eab63a2a
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-22 18:01:49 +0900

    [add]竿と魚のコンフィグ

---
+++ FisherLife/Assets/Scripts/Develop/Core/Data/FishConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Fish", menuName = "Config/Fish")]
+public class FishConfig : ScriptableObject
+{
+    public string Name;
+    public int Level;
+    [TextArea(0, 5)]
+    public string Description;
+}
---
+++ FisherLife/Assets/Scripts/Develop/Core/Data/RodConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Rod", menuName = "Config/RodConfig")]
+public class RodConfig : ScriptableObject
+{
+    public string Name;
+    public int Level;
+    [TextArea(0,5)]
+    public string Description;
+}
