e314ad77a68284dfbd6513594e102097f2b05330
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-03-29 08:07:28 +0900

    [update]Addressable経由でアセットを取得できるように変既往

--- FisherLife/Assets/Scripts/Develop/Mock/Typing/TypingSystem.cs
+++ FisherLife/Assets/Scripts/Develop/Mock/Typing/TypingSystem.cs
@@ -1,4 +1,7 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace Miyamoto.FisherLife.Develop.Mock.Typing
 {
@@ -7,12 +10,21 @@ namespace Miyamoto.FisherLife.Develop.Mock.Typing
     /// </summary>
     public class TypingSystem : MonoBehaviour
     {
-        [SerializeField] private TextAsset _textAsset;
+        [SerializeField] private AssetReferenceT<TextAsset> _textAssetReference;
         private LoadWords _loadWords;
+        private AsyncOperationHandle<TextAsset> _handle;
 
         private void Awake()
         {
-            _loadWords = new LoadWords(_textAsset);
+            LoadAsync().Forget();
+        }
+        
+        private async UniTask LoadAsync()
+        {
+            _handle = Addressables.LoadAssetAsync<TextAsset>(_textAssetReference);
+            await _handle.Task;
+            var textAsset = _handle.Result;
+            _loadWords = new LoadWords(textAsset);
         }
     }
 }
