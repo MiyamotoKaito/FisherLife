ecf1296b633d0d8253151946795b114e5c856808
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-03-30 16:42:45 +0900

    [add]クラス作成

---
+++ FisherLife/Assets/Scripts/Develop/Input/InputActions.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Miyamoto.FisherLife.Develop.Input
+{
+    public class InputActions : MonoBehaviour, IInputService
+    {
+        public event Action<string> OnType;
+        public event Action OnEnter;
+        private FisherLifeInputActions _inputActions;
+
+        private void Awake()
+        {
+            _inputActions = new FisherLifeInputActions();
+        }
+
+        private void OnEnable()
+        {
+            _inputActions.Enable();
+            _inputActions.Typing.KeyType.started += Type;
+            _inputActions.Typing.Enter.started += Enter;
+        }
+
+        private void OnDisable()
+        {
+            _inputActions.Disable();
+            _inputActions.Typing.KeyType.started -= Type;
+            _inputActions.Typing.Enter.started -= Enter;
+        }
+
+        private void Type(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+                OnType?.Invoke(Keyboard.current.ToString());
+            }
+        }
+
+        private void Enter(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+                OnEnter?.Invoke();
+            }
+        }
+    }
+}
