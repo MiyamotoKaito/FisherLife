ff61154cf5482abc3121116893114f8a593353ea
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-03-30 16:41:25 +0900

    [add]Viewに文字の削除と追加メソッドついあ

--- FisherLife/Assets/Scripts/Develop/Mock/Typing/TypingViewMock.cs
+++ FisherLife/Assets/Scripts/Develop/Mock/Typing/TypingViewMock.cs
@@ -6,5 +6,21 @@ namespace Miyamoto.FisherLife.Develop.Mock.Typing
     public class TypingViewMock : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _typingText;
+
+        /// <summary>
+        /// 文字追加
+        /// </summary>
+        /// <param name="text"></param>
+        public void AddText(string text)
+        {
+            _typingText.text += text;
+        }
+        /// <summary>
+        /// 一文字削除
+        /// </summary>
+        public void DeleteText()
+        {
+            _typingText.text.Remove(_typingText.text.Length - 1,1);
+        }
     }
 }
