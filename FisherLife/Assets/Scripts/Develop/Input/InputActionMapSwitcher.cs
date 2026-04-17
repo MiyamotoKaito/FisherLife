dd02bf136e1718c9efdfcb4b38a8fa4c3daa2473
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-04-17 22:05:10 +0900

    [update]アクションマップで入力切替を行うようにする。ここで切り替えをすれば他を見ないですむはず

---
+++ FisherLife/Assets/Scripts/Develop/Input/InputActionMapSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine.InputSystem;
+
+public class InputActionMapSwitcher : IDisposable
+{
+    private FisherLifeInputActions _inputActions;
+    private InputActionMap _currentActionMap;
+    public void ChangeContext(InputActionMap actionMap)
+    {
+        // 初回のアクションマップ設定
+        if (_currentActionMap == null)
+        {
+            _currentActionMap = actionMap;
+            _currentActionMap.Enable();
+        }
+
+        if (_currentActionMap != actionMap)
+        {
+            _currentActionMap?.Disable();
+
+            _currentActionMap = actionMap;
+            _currentActionMap.Enable();
+        }
+    }
+
+    public void Dispose()
+    {
+        _currentActionMap?.Disable();
+        _currentActionMap = null;
+    }
+}
