73f23afbef61e43c1f7c449c8c3e73d201999e9e
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-04-18 00:55:09 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Develop/Input/InputController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class InputContainer : MonoBehaviour 
+{
+    private void Awake()
+    {
+        if (this.gameObject != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+}
