e99e698dc8ab85cb2e68e0974e36ec35052810f4
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-05-05 23:28:04 +0900

    [add]クラス追加

--- FisherLife/Assets/Scripts/Develop/Input/Container.cs
+++ FisherLife/Assets/Scripts/Develop/Input/Container.cs
@@ -3,8 +3,11 @@ using VContainer.Unity;
 
 public class Container : LifetimeScope
 {
+
     protected override void Configure(IContainerBuilder builder)
     {
-         
+        builder.Register<FisherLifeInputActions>(Lifetime.Singleton);
+        builder.Register<InputActionMapSwitcher>(Lifetime.Singleton);
+        builder.Register<TypingInputHandler>(Lifetime.Singleton);
     }
 }
