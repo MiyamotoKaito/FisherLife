252dbc95ace558eb7c7397492fb3f87c71b981c3
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-06-26 18:13:42 +0900

    [add]タイピング専用のインプットクラス

---
+++ FisherLife/Assets/Scripts/Input/TypingInput.cs
@@ -0,0 +1,37 @@
+using System;
+using R3;
+using UnityEngine.InputSystem;
+
+namespace InputModule
+{
+    public class TypingInput : IDisposable
+    {
+        public TypingInput()
+        {
+            Keyboard.current.onTextInput += OnTextInput;
+        }
+        public Observable<char> OnChar => _onChar;
+        public void SetEnable(bool enable) => _enabled = enable;
+
+        private Subject<char> _onChar;
+
+        private bool _enabled;
+        public void Dispose()
+        {
+            _onChar?.Dispose();
+            _onChar = null;
+            if (Keyboard.current != null)
+            {
+                Keyboard.current.onTextInput -= OnTextInput;
+            }
+        }
+        private void OnTextInput(char c)
+        {
+            if (!_enabled) return;//有効ではなかったら早期リターン
+
+            if (char.IsControl(c)) return; // \b \n \r などを弾く
+
+            _onChar.OnNext(c);
+        }
+    }
+}
