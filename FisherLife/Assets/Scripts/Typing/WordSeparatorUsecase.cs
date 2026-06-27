0be33f6b65cbe2377beb2dbb602c6b4495875317
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 19:03:22 +0900

    [update]分割機能実装

--- FisherLife/Assets/Scripts/Typing/WordSeparatorUsecase.cs
+++ FisherLife/Assets/Scripts/Typing/WordSeparatorUsecase.cs
@@ -1,7 +1,35 @@
-﻿namespace TypingModule
+﻿using Common;
+using System.Collections.Generic;
+
+namespace TypingModule
 {
-    public class WordSeparatorUsecase
+    /// <summary>
+    /// 単語をレベル別に分けるUsecase
+    /// </summary>
+    public class WordSeparatorUsecase : IWordSeparatorUsecase
     {
-    
+        /// <summary>
+        /// 与えられたテキストファイルをカンマ区切りで辞書に代入する
+        /// </summary>
+        /// <param name="text"></param>
+        public Dictionary<uint, string[]> WordSeparate(string text)
+        {
+            var dictionaty = new Dictionary<uint, string[]>();
+
+            var lines = text.Split("\n");
+            foreach (var line in lines)
+            {
+                var num = line[0].ToString();//TryParse用にStringに変換
+                if (!uint.TryParse(num, out uint resule))
+                {
+                    continue;
+                }
+                var words = line.Split(",", line[0]);// カンマとレベルで区切る
+
+                dictionaty[resule] = words;
+            }
+
+            return dictionaty;
+        }
     }
 }
