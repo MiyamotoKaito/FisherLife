4ab58df2c61e812be82c6e37d504ec440b10d972
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 18:33:04 +0900

    [add]インターフェース追加

---
+++ FisherLife/Assets/Scripts/Common/IWordSeparatorUsecase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Common
+{
+    public interface IWordSeparatorUsecase
+    {
+        Dictionary<int, string[]> WordDictionary { get; }
+        void WordSeparate();
+    }
+}
