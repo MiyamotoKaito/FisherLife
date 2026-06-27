c9ecafded04cf9c0e1748319b5cb56bc0e834163
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 11:44:27 +0900

    [update]Viewの注入

--- FisherLife/Assets/Scripts/Typing/TypingPresenter.cs
+++ FisherLife/Assets/Scripts/Typing/TypingPresenter.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Linq;
 using System.Threading;
 using InputModule;
@@ -11,9 +11,10 @@ namespace TypingModule
     /// </summary>
     public class TypingPresenter : IDisposable
     {
-        public TypingPresenter(TypingInput typingInput)
+        public TypingPresenter(TypingInput typingInput, TypingView typingView)
         {
             _typingInput = typingInput;
+            _typingView = typingView;
             _answerModel = new AnswerModel();
             _questionModel = new QuestionModel();
             _tokenSource = new();
@@ -25,14 +26,18 @@ namespace TypingModule
         /// </summary>
         private void Subscribe()
         {
-            _typingInput.OnChar.Subscribe(c => 
+            _typingInput.OnChar.Subscribe(c =>
             {
-                _answerModel.Write(c); 
-
+                _answerModel.Write(c);
             }).RegisterTo(_tokenSource.Token);
 
-            _answerModel.Answer.Subscribe(c =>
-            _questionModel.AddChar(c.First())).RegisterTo(_tokenSource.Token);
+            _answerModel.Answer.Subscribe(str =>
+            {
+                _questionModel.AddChar(str.First());
+                _typingView.UpdateAnswer(str);
+            }).RegisterTo(_tokenSource.Token);
+
+            _questionModel.Question.Subscribe(_typingView.SetQuestion).RegisterTo(_tokenSource.Token);
         }
 
         public void Dispose()
@@ -44,6 +49,7 @@ namespace TypingModule
         private readonly TypingInput _typingInput;
         private readonly AnswerModel _answerModel;
         private readonly QuestionModel _questionModel;
+        private readonly TypingView _typingView;
         private CancellationTokenSource _tokenSource;
     }
 }
