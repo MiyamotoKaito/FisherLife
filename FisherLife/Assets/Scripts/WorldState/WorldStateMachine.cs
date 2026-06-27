7de4a37f7372a4d5f227121c973deb3b96e8f6c9
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 17:19:25 +0900

    [update]ワールドステートの実装

--- FisherLife/Assets/Scripts/WorldState/WorldStateMachine.cs
+++ FisherLife/Assets/Scripts/WorldState/WorldStateMachine.cs
@@ -1,19 +1,47 @@
+﻿using Common;
+using System.Collections.Generic;
 using UnityEngine;
+using VContainer;
 
 namespace WorldStateModule
 {
     public class WorldStateMachine : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
+        public static WorldStateMachine Instance;
+        [SerializeReference, SubclassSelector]
+        private IState[] _states;
+        private Dictionary<WorldStateType, IState> _stateDic = new();
 
-        // Update is called once per frame
-        void Update()
+        private IState _currentState;
+        private void Awake()
         {
-        
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            foreach (var state in _states)
+            {
+                _stateDic[state.WorldState] = state;
+            }
+        }
+        [Inject]
+        public void Init(IInputActionMapStack inputActionMapStack)
+        {
+            foreach (var (key, value) in _stateDic)
+            {
+                value.Init(inputActionMapStack);
+            }
+        }
+        public void ChangeState(WorldStateType worldStateType)
+        {
+            _currentState.Exit();
+            _currentState = _stateDic[worldStateType];
+            _currentState.Entry();
         }
     }
 }
