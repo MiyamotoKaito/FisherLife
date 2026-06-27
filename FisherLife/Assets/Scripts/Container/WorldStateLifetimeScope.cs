c068405b505ef4f1fb79647ab8c42bdb62e12511
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 16:19:22 +0900

    [add]ワーるとステート用のライフタイムスコープ

---
+++ FisherLife/Assets/Scripts/Container/WorldStateLifetimeScope.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+using VContainer;
+using VContainer.Unity;
+using UnityEngine;
+
+namespace WorldStateModule
+{
+    public class WorldStateLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterComponentInHierarchy<WorldStateMachine>();
+        }
+    }
+}
