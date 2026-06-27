449dafbe14007a783bd27dd7e56fbec95ec824dd
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 21:10:16 +0900

    [update]ロジック部分を削除

--- FisherLife/Assets/Scripts/Container/TypingLifeTimeScope.cs
+++ FisherLife/Assets/Scripts/Container/TypingLifeTimeScope.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Common;
 using InputModule;
 using TypingModule;
@@ -18,30 +18,15 @@ namespace Container
             // View(MonoBehaviour) を ITypingView として登録（シーン階層から取得）
             builder.RegisterComponentInHierarchy<TypingView>().As<ITypingView>();
 
+            builder.RegisterComponentInHierarchy<TypingController>();
+
             // 入力(ピュアクラス) を ITypingInput として登録
             builder.Register<TypingInput>(Lifetime.Singleton).As<ITypingInput>();
-
-            // Presenter(ピュアクラス)。VContainer に依存させたくないので
-            // EntryPoint(IStartable等) ではなく BuildCallback で起動する。
+            
+            // 
             builder.Register<TypingPresenter>(Lifetime.Singleton);
 
-            // --- 動作確認用の仮ドライバ（後で正式な出題ロジックに差し替える） ---
-            builder.RegisterBuildCallback(resolver =>
-            {
-                var presenter = resolver.Resolve<TypingPresenter>();
-                var questions = new Queue<string>(new[] { "fish", "rod", "sea" });
-
-                void Next()
-                {
-                    if (questions.Count > 0)
-                        presenter.StartTyping(questions.Dequeue());
-                    else
-                        presenter.StopTyping();
-                }
-
-                presenter.Completed += Next;
-                Next();
-            });
+            builder.Register<WordSeparatorUsecase>(Lifetime.Singleton).As<IWordSeparatorUsecase>();
         }
     }
 }
