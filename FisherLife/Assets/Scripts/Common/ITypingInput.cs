c2491ee4d496627b2293c6528794bb4a27399aa7
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 12:13:26 +0900

    [add]interface追加

---
+++ FisherLife/Assets/Scripts/Common/ITypingInput.cs
@@ -0,0 +1,11 @@
+﻿using R3;
+using System;
+
+namespace Common
+{
+    public interface ITypingInput : IDisposable
+    {
+        Observable<char> OnChar { get; }
+        void SetEnable(bool enable);
+    }
+}
