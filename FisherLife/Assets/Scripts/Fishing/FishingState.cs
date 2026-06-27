8083270ce203e46d648d3f96e61b227cdc84e5f3
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 16:19:48 +0900

    [update]フォルダ移動

---
+++ FisherLife/Assets/Scripts/Fishing/FishingState.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+
+namespace FishingModule
+{
+    [System.Serializable]
+    public class FishingState : IState
+    {
+        public WorldStateType WorldState => WorldStateType.Fishing;
+
+        public void Init(IInputActionMapStack inputActionMapStack)
+        {
+            _inputActionMapStack = inputActionMapStack;
+        }
+
+        public void Entry()
+        {
+            _inputActionMapStack.Push(InputActionMapType.Player);
+        }
+
+        public void Exit()
+        {
+            throw new System.NotImplementedException();
+        }
+        private IInputActionMapStack _inputActionMapStack;
+    }
+}
--- FisherLife/Assets/Scripts/WorldState/FishingState.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace WorldStateModule
-{
-    public class FishingState
-    {
-    
-    }
-}
