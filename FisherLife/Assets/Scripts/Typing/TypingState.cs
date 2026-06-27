80f73b11c94ed450091921a524dc998d55d309da
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 17:24:19 +0900

    [update]タイピングState実装

--- FisherLife/Assets/Scripts/Typing/TypingState.cs
+++ FisherLife/Assets/Scripts/Typing/TypingState.cs
@@ -1,9 +1,26 @@
+﻿using Common;
 using UnityEngine;
 
 namespace TypingModule
 {
-    public class TypingState
+    public class TypingState : IState
     {
-    
+        public WorldStateType WorldState => WorldStateType.Typing;
+        public void Init(IInputActionMapStack inputActionMapStack)
+        {
+            _inputActionMapStack = inputActionMapStack;
+        }
+
+        public void Entry()
+        {
+            _inputActionMapStack.Push(InputActionMapType.Typing);
+        }
+
+        public void Exit()
+        {
+            _inputActionMapStack.Pop();
+        }
+
+        private IInputActionMapStack _inputActionMapStack;
     }
 }
