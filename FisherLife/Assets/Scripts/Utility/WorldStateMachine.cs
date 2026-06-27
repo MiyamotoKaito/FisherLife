81b933621cc5267fe7d43ef2047161809a31fb19
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 21:12:05 +0900

    [update]Utilityに移動

--- FisherLife/Assets/Scripts/Utility/WorldStateMachine.cs
+++ FisherLife/Assets/Scripts/Utility/WorldStateMachine.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using VContainer;
 
-namespace WorldStateModule
+namespace Utility
 {
-    public class WorldStateMachine : MonoBehaviour, IWorldStateMachine
+    public class WorldStateMachine : MonoBehaviour
     {
         public static WorldStateMachine Instance;
         [SerializeReference, SubclassSelector]
         private IState[] _states;
-        private Dictionary<WorldStateType, IState> _stateDic = new();
+        private Dictionary<WorldStateType, IState> _stateDic;
 
         private IState _currentState;
+        private IInputActionMapStack _inputActionMapStack;
         private void Awake()
         {
             if (Instance == null)
@@ -24,22 +25,24 @@ namespace WorldStateModule
             {
                 Destroy(gameObject);
             }
+            _stateDic = new Dictionary<WorldStateType, IState>();
             foreach (var state in _states)
             {
+                state.Init(_inputActionMapStack);
                 _stateDic[state.WorldState] = state;
             }
         }
         [Inject]
         public void Init(IInputActionMapStack inputActionMapStack)
         {
-            foreach (var (key, value) in _stateDic)
-            {
-                value.Init(inputActionMapStack);
-            }
+            _inputActionMapStack = inputActionMapStack;
         }
         public void ChangeState(WorldStateType worldStateType)
         {
-            _currentState.Exit();
+            if (_currentState != null)
+            {
+                _currentState.Exit();
+            }
             _currentState = _stateDic[worldStateType];
             _currentState.Entry();
         }
