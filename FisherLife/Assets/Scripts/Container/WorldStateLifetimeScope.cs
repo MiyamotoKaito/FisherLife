e459194051a6a71d30bdc88ad40e56159c154bd0
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 17:21:23 +0900

    [update]アクションアセットとアクションマップのスタックを登録

--- FisherLife/Assets/Scripts/Container/WorldStateLifetimeScope.cs
+++ FisherLife/Assets/Scripts/Container/WorldStateLifetimeScope.cs
@@ -1,14 +1,19 @@
 ﻿using Common;
+using InputModule;
+using UnityEngine;
+using UnityEngine.InputSystem;
 using VContainer;
 using VContainer.Unity;
-using UnityEngine;
 
 namespace WorldStateModule
 {
     public class WorldStateLifetimeScope : LifetimeScope
     {
+        [SerializeField] private InputActionAsset _action;
         protected override void Configure(IContainerBuilder builder)
         {
+            builder.RegisterInstance(_action);
+            builder.Register<InputActionMapStack>(Lifetime.Singleton).As<IInputActionMapStack>();
             builder.RegisterComponentInHierarchy<WorldStateMachine>();
         }
     }
