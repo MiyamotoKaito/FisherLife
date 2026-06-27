c9033dacde2f94ac248f698c30cccd987cec07cf
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 12:16:00 +0900

    [update]インターフェースの継承と関数の実装

--- FisherLife/Assets/Scripts/Typing/TypingView.cs
+++ FisherLife/Assets/Scripts/Typing/TypingView.cs
@@ -1,3 +1,4 @@
+﻿using Common;
 using TMPro;
 using UnityEngine;
 namespace TypingModule
@@ -5,8 +6,16 @@ namespace TypingModule
     /// <summary>
     /// タイピングのUI管理のViewクラス
     /// </summary>
-    public class TypingView : MonoBehaviour
+    public class TypingView : MonoBehaviour, ITypingView
     {
+        public void SetQuestion(string q)
+        {
+            _question.text = q;
+        }
+        public void UpdateAnswer(string str)
+        {
+            _answer.text = str;
+        }
         [SerializeField] private TextMeshProUGUI _answer;
         [SerializeField] private TextMeshProUGUI _question;
     }
