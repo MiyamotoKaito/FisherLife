feca085ad70c51b3d22c4f6bac6caab3b9ccabfb
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-27 12:53:54 +0900

    [delete]TypingInput は今やコンテナが破棄するので不要になった

--- FisherLife/Assets/Scripts/Typing/TypingPresenter.cs
+++ FisherLife/Assets/Scripts/Typing/TypingPresenter.cs
@@ -43,7 +43,6 @@ namespace TypingModule
         public void Dispose()
         {
             _tokenSource?.Cancel();
-            _typingInput?.Dispose();
         }
 
         private readonly AnswerModel _answerModel;
