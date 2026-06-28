23adffece87e01d695992d56d76b5f09cd739734
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 06:18:30 +0900

    [add]InGameのLifetimeScope

---
+++ FisherLife/Assets/Scripts/Container/InGameLifetimeScope.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using VContainer;
+using VContainer.Unity;
+
+namespace Container
+{
+    public class InGameLifetimeScope : LifetimeScope
+    {
+        [SerializeField] private InputActionAsset _actionAsset;
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterInstance(_actionAsset);
+        }
+    }
+}
