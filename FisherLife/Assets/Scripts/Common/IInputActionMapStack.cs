68fbbdad4ae551f9f2dafa354fd93e29c3237775
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 06:13:23 +0900

    [update]現在のアクションマップを見れるように設定

--- FisherLife/Assets/Scripts/Common/IInputActionMapStack.cs
+++ FisherLife/Assets/Scripts/Common/IInputActionMapStack.cs
@@ -1,7 +1,10 @@
-﻿namespace Common
+﻿using UnityEngine.InputSystem;
+
+namespace Common
 {
     public interface IInputActionMapStack
     {
+        InputActionMap CurrentMap { get; }
         void Pop();
         void Push(InputActionMapType mapType);
     }
