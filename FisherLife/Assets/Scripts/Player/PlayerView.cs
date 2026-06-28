eb34aadc8abcc4cebd31efdd18e3e12c502b9c1e
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 07:29:11 +0900

    [add]PlayerViewクラス

---
+++ FisherLife/Assets/Scripts/Player/PlayerView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using VContainer;
+
+namespace PlayerModule
+{
+    public class PlayerView : MonoBehaviour
+    {
+
+        private InputActionAsset _actionAsset;
+        [Inject]
+        public void Inject(InputActionAsset inputActions)
+        {
+            _actionAsset = inputActions;
+        }
+        private void Start()
+        {
+           
+        }
+    }
+}
