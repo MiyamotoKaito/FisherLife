0ad5366edac942f22426ffb3e85594196ff31c34
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 06:18:44 +0900

    [add]すべての親のLifetimeScope

---
+++ FisherLife/Assets/Scripts/Container/RootLifetimeScope.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+using InputModule;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Utility;
+using VContainer;
+using VContainer.Unity;
+
+namespace Container
+{
+    public class RootLifetimeScope : LifetimeScope
+    {
+        [SerializeField] private InputActionAsset _action;
+        protected override void Configure(IContainerBuilder builder)
+        {
+            DontDestroyOnLoad(this.gameObject);
+
+            builder.RegisterInstance(_action);
+            builder.Register<IInputActionMapStack, InputActionMapStack>(Lifetime.Singleton);
+            builder.RegisterComponentInHierarchy<WorldStateMachine>();
+        }
+    }
+}
