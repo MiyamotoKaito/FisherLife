20d3eaeca726aaf7ac1fefb22ab364cd79031462
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 07:44:23 +0900

    [update]アニメーション用のメソッド用意

--- FisherLife/Assets/Scripts/Player/PlayerView.cs
+++ FisherLife/Assets/Scripts/Player/PlayerView.cs
@@ -4,18 +4,37 @@ using VContainer;
 
 namespace PlayerModule
 {
+    [RequireComponent(typeof(Rigidbody), typeof(Animator))]
     public class PlayerView : MonoBehaviour
     {
-
+        private Rigidbody _rb;
+        private Animator _animator;
         private InputActionAsset _actionAsset;
         [Inject]
         public void Inject(InputActionAsset inputActions)
         {
             _actionAsset = inputActions;
         }
+        public void Move(Vector3 dir)
+        {
+            _rb.linearVelocity = dir;
+        }
+        public void Fishing()
+        {
+
+        }
+        public void ReturnFishing()
+        {
+
+        }
+        public void GetFish()
+        {
+
+        }
         private void Start()
         {
-           
+            _rb = GetComponent<Rigidbody>();
+            _animator = GetComponent<Animator>();
         }
     }
 }
