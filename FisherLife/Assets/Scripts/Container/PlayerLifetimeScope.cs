c507acbabe6ebfe0f129734842e260f3a3fca206
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 07:30:06 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Container/PlayerLifetimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Container
+{
+    public class PlayerLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
