10992f4e3995e837219bdd0ddb2553c045f72b22
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 21:30:19 +0900

    [update]InstaceをTypingLifetimeScopeに移動

--- FisherLife/Assets/Scripts/Container/InGameLifetimeScope.cs
+++ FisherLife/Assets/Scripts/Container/InGameLifetimeScope.cs
@@ -11,7 +11,6 @@ namespace Container
 {
     public class InGameLifetimeScope : LifetimeScope
     {
-        [SerializeField] private TextAsset _textAsset;
         protected override void Configure(IContainerBuilder builder)
         {
             #region Stateの登録
