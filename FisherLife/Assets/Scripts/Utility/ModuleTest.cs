640d55a52d9e09d6b14573de8b6d235c49e48e56
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 21:29:54 +0900

    [update]Module移動

---
+++ FisherLife/Assets/Scripts/Utility/ModuleTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using VContainer;
+
+namespace Utility
+{
+    public class ModuleTest : MonoBehaviour
+    {
+        [Inject]
+        public void Inject()
+        {
+
+        }
+        private void Awake()
+        {
+            
+        }
+    }
+}
