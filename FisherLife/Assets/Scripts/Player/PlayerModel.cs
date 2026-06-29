e8d0c09854a8877df358328b44310a71396366b9
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 21:22:59 +0900

    [update]PlayerModelの実装

--- FisherLife/Assets/Scripts/Player/PlayerModel.cs
+++ FisherLife/Assets/Scripts/Player/PlayerModel.cs
@@ -1,9 +1,22 @@
-﻿using UnityEngine;
+﻿using R3;
+using UnityEngine;
 
 namespace PlayerModule
 {
+    /// <summary>
+    ///  プレイヤーのモデル
+    /// </summary>
     public class PlayerModel
     {
-        
+        public ReadOnlyReactiveProperty<Vector3> MoveDirection => _dir;
+        private ReactiveProperty<Vector3> _dir;
+        /// <summary>
+        /// 入力された方向を設定する
+        /// </summary>
+        /// <param name="dir">入力された方向</param>
+        public void SetDirection(Vector3 dir)
+        {
+            _dir.Value = dir;
+        }
     }
 }
