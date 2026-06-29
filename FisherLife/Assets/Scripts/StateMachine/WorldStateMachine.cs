7800b862d81e77f6bbdf1c9ab160a0a1932a962a
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 09:52:45 +0900

    [update]Utilityではなくなったためフォルダ移動

---
+++ FisherLife/Assets/Scripts/StateMachine/WorldStateMachine.cs
@@ -0,0 +1,47 @@
+﻿using Commons;
+using System.Collections.Generic;
+
+namespace StateMachine
+{
+    public class WorldStateMachine : IWorldStateMachine
+    {
+        private IState _currentState;
+        private Dictionary<WorldStateType, IState> _stateDic;
+        private Stack<IState> _stateStack;
+
+        public WorldStateMachine(IState[] states)
+        {
+            Init(states);
+        }
+        public void ChangeState(WorldStateType worldStateType)
+        {
+            if (_stateStack.Count > 0)
+            {
+                var lastState = _stateStack.Pop();
+            }
+            _currentState = _stateDic[worldStateType];
+            _currentState.Entry();
+            _stateStack.Push(_currentState);
+        }
+
+        public void BackState()
+        {
+            if (_stateStack.Count <= 0)
+            {
+                return;
+            }
+            _currentState = _stateStack.Pop();
+
+            _currentState.Exit();
+        }
+        private void Init(IState[] states)
+        {
+            _stateDic = new Dictionary<WorldStateType, IState>();
+            _stateStack = new Stack<IState>();
+            foreach (var state in states)
+            {
+                _stateDic[state.WorldState] = state;
+            }
+        }
+    }
+}
