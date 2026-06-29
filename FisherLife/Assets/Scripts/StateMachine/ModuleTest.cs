26e6b66b082dc2df88fecaccb7bff8bedb29e5a2
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 21:25:57 +0900

    [add]ModuleTest用のクラス作成

---
+++ FisherLife/Assets/Scripts/StateMachine/ModuleTest.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace StateMachine
+{
+    public class ModuleTest : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
