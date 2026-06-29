f6cff74dcf83b9735cc1295b0bb14690d7524879
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 21:24:22 +0900

    [update]PlayerViewの実装

--- FisherLife/Assets/Scripts/Player/PlayerView.cs
+++ FisherLife/Assets/Scripts/Player/PlayerView.cs
@@ -1,18 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
-using VContainer;
 
 namespace PlayerModule
 {
     [RequireComponent(typeof(Rigidbody), typeof(Animator))]
     public class PlayerView : MonoBehaviour
     {
+        [SerializeField] private PlayerParametor _parametor;
         private Rigidbody _rb;
         private Animator _animator;
         private InputActionAsset _actionAsset;
+        private Vector3 _direction;
         public void Move(Vector3 dir)
         {
-            _rb.linearVelocity = dir;
+            _direction = dir;
         }
         public void Fishing()
         {
@@ -30,6 +31,12 @@ namespace PlayerModule
         {
             _rb = GetComponent<Rigidbody>();
             _animator = GetComponent<Animator>();
+        }
+        private void FixedUpdate()
+        {
+            var v = _direction * _parametor.MoveSpeed;
+
+            _rb.linearVelocity = new Vector3(v.x, _rb.linearVelocity.y, v.z);
         }
     }
 }
