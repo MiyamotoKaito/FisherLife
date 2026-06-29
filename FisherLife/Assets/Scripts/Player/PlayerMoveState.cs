27f7fe72bf07d9f107c080f8dbd6ef262d80c9bc
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 09:53:23 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Player/PlayerMoveState.cs
@@ -0,0 +1,28 @@
+﻿using Commons;
+using System;
+
+namespace PlayerModule
+{
+    public class PlayerMoveState : IState
+    {
+        public PlayerMoveState(IInputActionMapStack inputActionMapStack, PlayerController playerController)
+        {
+            _inputMapStack = inputActionMapStack;
+            _playerController = playerController;
+        }
+        public WorldStateType WorldState => WorldStateType.Moving;
+
+        public event Action OnStateChange;
+        private readonly IInputActionMapStack _inputMapStack;
+        private readonly PlayerController _playerController;
+        public void Entry()
+        {
+            _inputMapStack.Push(InputActionMapType.Player);
+        }
+
+        public void Exit()
+        {
+            _inputMapStack.Pop();
+        }
+    }
+}
