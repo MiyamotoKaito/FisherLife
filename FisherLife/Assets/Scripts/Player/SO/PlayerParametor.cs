8674fba3eddf99db5e596d0cb9dbf2242c0cfa46
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 10:52:34 +0900

    [add]SO追加

---
+++ FisherLife/Assets/Scripts/Player/SO/PlayerParametor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace PlayerModule
+{
+    [CreateAssetMenu(fileName = "PlayerParametor", menuName = "Scriptable Objects/PlayerParametor")]
+    public class PlayerParametor : ScriptableObject
+    {
+        public int MoveSpeed;
+    }
+}
