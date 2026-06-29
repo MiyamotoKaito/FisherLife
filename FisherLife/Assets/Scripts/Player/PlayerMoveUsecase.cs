8b9f190eecfde3623404ceffc103c3754078f9cd
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 10:45:21 +0900

    [add]Usecase追加

---
+++ FisherLife/Assets/Scripts/Player/PlayerMoveUsecase.cs
@@ -0,0 +1,18 @@
+﻿using Commons;
+using UnityEngine;
+
+namespace PlayerModule
+{
+    public class PlayerMoveUsecase : IPlayerMoveUsecase
+    {
+        /// <summary>
+        /// InputSystemの渡されたVector2をVector3に変換する
+        /// </summary>
+        /// <param name="readValue"></param>
+        /// <returns></returns>
+        public Vector3 ToVector3(Vector2 readValue)
+        {
+            return new Vector3(readValue.x, 0, readValue.y); 
+        }
+    }
+}
