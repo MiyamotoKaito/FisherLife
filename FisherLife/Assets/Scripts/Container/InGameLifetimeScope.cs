cdd883f649bf62674383b3d23724e9b6073776eb
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 09:54:10 +0900

    [update]InGameLifetimeScopeにまとめる

--- FisherLife/Assets/Scripts/Container/InGameLifetimeScope.cs
+++ FisherLife/Assets/Scripts/Container/InGameLifetimeScope.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
-using UnityEngine.InputSystem;
+﻿using Commons;
+using InputModule;
+using PlayerModule;
+using StateMachine;
+using TypingModule;
 using VContainer;
 using VContainer.Unity;
 
@@ -7,10 +10,27 @@ namespace Container
 {
     public class InGameLifetimeScope : LifetimeScope
     {
-        [SerializeField] private InputActionAsset _actionAsset;
         protected override void Configure(IContainerBuilder builder)
         {
-            builder.RegisterInstance(_actionAsset);
+            builder.RegisterComponentInHierarchy<WorldStateMachine>().As<IWorldStateMachine>();
+            #region PlayerMoveの登録
+            builder.RegisterComponentInHierarchy<PlayerView>();
+            #endregion
+
+            #region Typingの登録
+            // View(MonoBehaviour) を ITypingView として登録（シーン階層から取得）
+            builder.RegisterComponentInHierarchy<TypingView>().As<ITypingView>();
+
+            builder.RegisterComponentInHierarchy<TypingController>();
+
+            // 入力(ピュアクラス) を ITypingInput として登録
+            builder.Register<ITypingInput, TypingInput>(Lifetime.Singleton);
+
+            // 
+            builder.Register<TypingPresenter>(Lifetime.Singleton);
+
+            builder.Register<IWordSeparatorUsecase, CSVSeparatorUsecase>(Lifetime.Singleton);
+            #endregion
         }
     }
 }
