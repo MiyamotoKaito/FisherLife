30c6cc55981c93e0e7b2f781a3af45b0d6a499eb
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 21:24:36 +0900

    [update]PlayerControllerの実装

--- FisherLife/Assets/Scripts/Player/PlayerController.cs
+++ FisherLife/Assets/Scripts/Player/PlayerController.cs
@@ -1,12 +1,49 @@
-﻿using UnityEngine;
+﻿using Commons;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace PlayerModule
 {
-    public class PlayerController
+    public class PlayerController : IPlayerController
     {
-        public PlayerController()
+        public PlayerController(InputActionAsset inputActions,
+            PlayerMovePresenter playerMovePresenter,
+            IPlayerMoveUsecase playerMoveUsecase)
         {
+            _inputActions = inputActions;
+            _playerMovePresenter = playerMovePresenter;
+            _playerMoveUsecase = playerMoveUsecase;
 
+            _playerMap = _inputActions.FindActionMap(InputActionMapType.ToString(), true);
+            _moveAction = _playerMap.FindAction("Move");
+        }
+        public InputActionMapType InputActionMapType => InputActionMapType.Player;
+        private readonly InputActionAsset _inputActions;
+        private readonly InputActionMap _playerMap;
+        private readonly InputAction _moveAction;
+        private readonly PlayerMovePresenter _playerMovePresenter;
+        private readonly IPlayerMoveUsecase _playerMoveUsecase;
+        public void Enable()
+        {
+            _moveAction.performed += OnMove;
+            _moveAction.canceled += OnMove;
+        }
+
+        public void Disable()
+        {
+            _moveAction.performed -= OnMove;
+            _moveAction.canceled -= OnMove;
+        }
+
+        public void Dispose()
+        {
+            Disable();
+        }
+        private void OnMove(InputAction.CallbackContext callbackContext)
+        {
+            var readValue = callbackContext.ReadValue<Vector2>();
+            var dir = _playerMoveUsecase.ToVector3(readValue);
+            _playerMovePresenter.SetMove(dir);
         }
     }
 }
