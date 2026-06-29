1c2284f6093185db84af98c3164776878c62705b
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-29 21:21:57 +0900

    [add]クラス追加　ビルド時のコールバックでStateを登録する方式に変更

---
+++ FisherLife/Assets/Scripts/Container/TypingLifetimeScope.cs
@@ -0,0 +1,36 @@
+﻿using Commons;
+using InputModule;
+using TypingModule;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Container
+{
+    public class TypingLifetimeScope : LifetimeScope
+    {
+        [SerializeField] private TextAsset _textAsset;
+        protected override void Configure(IContainerBuilder builder)
+        {
+            // View(MonoBehaviour) を ITypingView として登録（シーン階層から取得）
+            builder.RegisterComponentInHierarchy<TypingView>().As<ITypingView>();
+
+            builder.RegisterComponentInHierarchy<TypingController>();
+
+            // 入力(ピュアクラス) を ITypingInput として登録
+            builder.Register<ITypingInput, TypingInput>(Lifetime.Singleton);
+            builder.RegisterInstance(_textAsset);
+            // 
+            builder.Register<TypingPresenter>(Lifetime.Singleton);
+
+            builder.Register<IWordSeparatorUsecase, CSVSeparatorUsecase>(Lifetime.Singleton);
+
+            builder.Register<IState, TypingState>(Lifetime.Singleton);
+            builder.RegisterBuildCallback(resolver =>
+            {
+                var machine = resolver.Resolve<IWorldStateMachine>();
+                machine.AddState(resolver.Resolve<TypingState>());
+            });
+        }
+    }
+}
