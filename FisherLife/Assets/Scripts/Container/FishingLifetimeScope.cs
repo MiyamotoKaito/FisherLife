1f9dbebb874461ea4ebb7bf717aab57bdc3c9ece
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-06-30 13:47:40 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Container/FishingLifetimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Container
+{
+    public class FishingLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
