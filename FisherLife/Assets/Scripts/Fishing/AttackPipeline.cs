145537c06c54aa6e6cb70ae30dde1bb53d4023e2
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-06-30 16:22:03 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Fishing/AttackPipeline.cs
@@ -0,0 +1,21 @@
+using Commons;
+using UnityEngine;
+
+namespace FishingModule
+{
+    public class AttackPipeline : IAttackPipeline
+    {
+        public AttackPipeline(IAttackCalculator attackCalculator)
+        {
+            _attackCalculator = attackCalculator;
+        }
+        public AttackResult Attack(IAttacker attacker, IDamageable target)
+        {
+            var result = _attackCalculator.Calculate(attacker, target);
+            target.TakeDamage(result.Damage);
+            return result;
+        }
+
+        private readonly IAttackCalculator _attackCalculator;
+    }
+}
