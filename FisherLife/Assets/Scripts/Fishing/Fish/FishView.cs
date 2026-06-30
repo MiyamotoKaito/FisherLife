b579203dc860590149b4ae1be929e062f071dfe8
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-06-30 13:53:30 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Fishing/Fish/FishView.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace FishingModule
+{
+    public class FishView : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
