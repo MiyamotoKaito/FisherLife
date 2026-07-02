45cce8002f7cb1b0bde85a1affba3309596ba065
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-07-02 18:44:50 +0900

    [update]Registryに依存するように変更

--- FisherLife/Assets/Scripts/Battle/BattleUsecase.cs
+++ FisherLife/Assets/Scripts/Battle/BattleUsecase.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading;
 using Commons;
 using Cysharp.Threading.Tasks;
@@ -6,34 +5,33 @@ using R3;
 
 namespace BattleModule
 {
-    public class BattleUsecase
+    public class BattleUsecase : IBattleUsecase
     {
         public BattleUsecase(IAttackPipeline attackPipeline,
-            float timeLimit,
-            IWorldStateMachine worldStateMachine)
+            IWorldStateMachine worldStateMachine,
+            IFishingModeRegistry fishingModeRegistry)
         {
             _attackPipeline = attackPipeline;
-            _timeLimit = timeLimit;
-            _worldStateMachine = worldStateMachine;
+            _timeLimit = 10;
+            _fishingModeRegistry = fishingModeRegistry;
             _cancellationTokenSource = new CancellationTokenSource();
         }
         public async UniTask<BattleResult> BattleStart(IAttacker attacker, IDamageable target)
         {
-            using var _ = _fishingModeController.OnAttack.
+            using var _ = _fishingModeRegistry.GetMode(FishingMode.Typing).OnAttack.
                 Subscribe(_ => _attackPipeline.Attack(attacker, target)).
                 RegisterTo(_cancellationTokenSource.Token);
 
-            var dead = target.Hp.FirstAsync(hp => hp <= 0);
-            var timeOut = UniTask.Delay(TimeSpan.FromSeconds(_timeLimit));
+            var dead = target.Hp.FirstAsync(hp => hp <= 0, _cancellationTokenSource.Token);
+            var timeOut = UniTask.Delay((int)(_timeLimit * 1000), cancellationToken: _cancellationTokenSource.Token);
 
+            //　どちらかが先に完了するまで待機する
             await UniTask.WhenAny(dead.AsUniTask(), timeOut);
-            _worldStateMachine.BackState();
             return target.Hp.Value <= 0 ? BattleResult.Caught : BattleResult.Escaped;
         }
         private readonly float _timeLimit;
-        private readonly IWorldStateMachine _worldStateMachine;
         private readonly IAttackPipeline _attackPipeline;
-        private readonly IFishingModeController _fishingModeController;
+        private readonly IFishingModeRegistry _fishingModeRegistry;
         private readonly CancellationTokenSource _cancellationTokenSource;
     }
 }
