d61af990997fe6ba68d263fd4027d48fd262ce78
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-07-02 18:45:08 +0900

    [update]クリティカル変数要らなかった

--- FisherLife/Assets/Scripts/Common/AttackResult.cs
+++ FisherLife/Assets/Scripts/Common/AttackResult.cs
@@ -7,16 +7,12 @@ namespace Commons
     {
         /// <summary>実際のダメージ</summary>
         public int Damage => _damage;
-        /// <summary>クリティカル判定</summary>
-        public bool IsCritical => _isCritical;
 
-        public AttackResult(int damage, bool isCritical)
+        public AttackResult(int damage)
         {
             _damage = damage;
-            _isCritical = isCritical;
         }
 
         private readonly int _damage;
-        private readonly bool _isCritical;
     }
 }
