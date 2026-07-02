b6d8de4e5ea59df9bea13e9f33dc9a69bd354516
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-07-02 18:46:56 +0900

    [update]FishingLifetimeScope実装

--- FisherLife/Assets/Scripts/Container/FishingLifetimeScope.cs
+++ FisherLife/Assets/Scripts/Container/FishingLifetimeScope.cs
@@ -1,3 +1,7 @@
+using Commons;
+using FishingModule;
+using FishModule;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
@@ -8,11 +12,23 @@ namespace Container
     /// </summary>
     public class FishingLifetimeScope : LifetimeScope
     {
+        [SerializeField] private FishListAsset _fishListAsset;
         /// <summary>
         ///     釣り関連の依存を登録する。
         /// </summary>
         protected override void Configure(IContainerBuilder builder)
         {
+            builder.RegisterInstance(_fishListAsset);
+            builder.Register<IFishingController,FishingController>(Lifetime.Singleton);
+            builder.RegisterComponentInHierarchy<FishFactory>().As<IFishFactory>();
+            builder.Register<FishingState>(Lifetime.Singleton);
+            builder.RegisterBuildCallback(resolver =>
+            {
+                var machine = resolver.Resolve<IWorldStateMachine>();
+                machine.AddState(resolver.Resolve<FishingState>());
+                var ListAsset = resolver.Resolve<FishListAsset>();
+                ListAsset.InitDictionaries();
+            });
         }
     }
 }
