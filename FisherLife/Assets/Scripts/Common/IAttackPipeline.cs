ab546074b27e5484345ee41db418ce6d8bed6626
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-07-02 18:45:33 +0900

    [update]IDisposable削除

--- FisherLife/Assets/Scripts/Common/IAttackPipeline.cs
+++ FisherLife/Assets/Scripts/Common/IAttackPipeline.cs
@@ -5,11 +5,11 @@ namespace Commons
     /// <summary>
     ///     攻撃処理のパイプラインのインターフェース。
     /// </summary>
-    public interface IAttackPipeline : IDisposable
+    public interface IAttackPipeline
     {
         /// <summary>
         ///     攻撃側の能力をもとに、対象へ攻撃を計算して適用する。
         /// </summary>
-        AttackResult Attack(IAttacker attacker, IDamageable target);
+        void Attack(IAttacker attacker, IDamageable target);
     }
 }
