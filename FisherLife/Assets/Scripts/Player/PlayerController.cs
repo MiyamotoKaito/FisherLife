e0273e1bcb5c457ca9a9e28c50679620908f10f7
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-07-04 18:30:30 +0900

    [add]インタラクト時にイベント発火するためにIWorldStateMachineインターフェースを参照

--- FisherLife/Assets/Scripts/Player/PlayerController.cs
+++ FisherLife/Assets/Scripts/Player/PlayerController.cs
@@ -14,15 +14,18 @@ namespace PlayerModule
         /// </summary>
         public PlayerController(InputActionAsset inputActions,
             PlayerMovePresenter playerMovePresenter,
-            IPlayerMoveUsecase playerMoveUsecase)
+            IPlayerMoveUsecase playerMoveUsecase,
+            IWorldStateMachine worldStateMachine)
         {
             _inputActions = inputActions;
             _playerMovePresenter = playerMovePresenter;
             _playerMoveUsecase = playerMoveUsecase;
+            _worldStateMachine = worldStateMachine;
 
             // 同名プロパティ（InputActionMapType.Player）の文字列でマップを取得する。
             _playerMap = _inputActions.FindActionMap(InputActionMapType.ToString(), true);
             _moveAction = _playerMap.FindAction(MOVE_ACTION);
+            _interactAction = _playerMap.FindAction(INTERACT_ACTION);
         }
 
         /// <summary> 対応するアクションマップ種別。 </summary>
@@ -35,6 +38,7 @@ namespace PlayerModule
         {
             _moveAction.performed += MoveHandler;
             _moveAction.canceled += MoveHandler;
+            _interactAction.started += InteractHandler;
         }
 
         /// <summary>
@@ -44,6 +48,7 @@ namespace PlayerModule
         {
             _moveAction.performed -= MoveHandler;
             _moveAction.canceled -= MoveHandler;
+            _interactAction.started -= InteractHandler;
         }
 
         /// <summary>
@@ -55,12 +60,15 @@ namespace PlayerModule
         }
 
         private const string MOVE_ACTION = "Move";
+        private const string INTERACT_ACTION = "Interact";
 
         private readonly InputActionAsset _inputActions;
         private readonly InputActionMap _playerMap;
         private readonly InputAction _moveAction;
+        private readonly InputAction _interactAction;
         private readonly PlayerMovePresenter _playerMovePresenter;
         private readonly IPlayerMoveUsecase _playerMoveUsecase;
+        private readonly IWorldStateMachine _worldStateMachine;
 
         /// <summary>
         ///     移動入力を受け取り、変換した方向をPresenterへ渡す。
@@ -70,6 +78,14 @@ namespace PlayerModule
             var readValue = callbackContext.ReadValue<Vector2>();
             var dir = _playerMoveUsecase.ToVector3(readValue);
             _playerMovePresenter.SetMove(dir);
+        }
+
+        private void InteractHandler(InputAction.CallbackContext callbackContext)
+        {
+            if (callbackContext.started)
+            {
+                _worldStateMachine.ChangeState(WorldStateType.Fishing);
+            }
         }
     }
 }
