4bddaf53443a6bb92f2f7ab10a11d0fb679f9570
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-06 12:53:59 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Utility/FishingPriorityChange.cs
@@ -0,0 +1,10 @@
+﻿namespace Utility
+{
+    /// <summary>
+    ///     釣りが始まった時のCinemachineの優先度を設定する
+    /// </summary>
+    public class FishingPriorityChange : CinemachineCameraPrioriyChanger
+    {
+
+    }
+}
