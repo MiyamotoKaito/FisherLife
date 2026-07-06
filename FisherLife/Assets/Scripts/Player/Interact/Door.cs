fd9b6afa73ca0f3b77efb7374924080a06ec6fb5
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-06 12:46:29 +0900

    [update]ドアの実装

--- FisherLife/Assets/Scripts/Player/Interact/Door.cs
+++ FisherLife/Assets/Scripts/Player/Interact/Door.cs
@@ -1,12 +1,49 @@
-﻿using UnityEngine;
+﻿using Commons;
+using UnityEngine;
+using VContainer;
 
 namespace PlayerModule
 {
     public class Door : InteractObjectBase
     {
+        /// <summary>
+        /// ドアを開ける処理を行う
+        /// ここでShopに遷移する処理を追加する
+        /// </summary>
         public override void Interact()
         {
             Debug.Log("ドアを開ける");
+            _worldStateMachine.AllStop();
         }
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+            _interactionCanvas.enabled = false;
+        }
+        private void Update()
+        {
+            if (_interactionCanvas.enabled)
+            {
+                _interactionCanvas.transform.LookAt(_mainCamera.transform);
+                _interactionCanvas.transform.Rotate(0, 180, 0);
+            }
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _interactionCanvas.enabled = true;
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _interactionCanvas.enabled = false;
+            }
+        }
+        [Inject] private IWorldStateMachine _worldStateMachine;
+        [SerializeField] private Canvas _interactionCanvas;
+        private Camera _mainCamera;
     }
 }
