8e360996388820d0f5c44d7ab729b241334596e9
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-06 09:23:46 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Player/PlayerFishingPresenter.cs
@@ -0,0 +1,16 @@
+﻿namespace PlayerModule
+{
+    public class PlayerFishingPresenter
+    {
+        public PlayerFishingPresenter(PlayerView playerView)
+        {
+            _playerView = playerView;
+        }
+
+        public bool CanFishing()
+        {
+            return _playerView.CanFishing;
+        }
+        private readonly PlayerView _playerView;
+    }
+}
