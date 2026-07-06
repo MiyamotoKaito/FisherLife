53b232aa9bda565d8ccdc19a3cf9967bbae59b50
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-06 23:24:26 +0900

    [update]FishingAreaの実装

--- FisherLife/Assets/Scripts/Player/Interact/FishingArea.cs
+++ FisherLife/Assets/Scripts/Player/Interact/FishingArea.cs
@@ -1,4 +1,5 @@
 ﻿using Commons;
+using UnityEngine;
 using VContainer;
 
 namespace PlayerModule
@@ -7,9 +8,43 @@ namespace PlayerModule
     {
         public override void Interact()
         {
+            Debug.Log("釣り開始");
             _worldStateMachine.ChangeState(WorldStateType.Fishing);
         }
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+            _interactionCanvas.enabled = false;
+        }
+        private void Update()
+        {
+            if (_worldStateMachine.CurrentState != WorldStateType.Moving)
+            {
+                _interactionCanvas.enabled = false;
+            }
 
+            if (_interactionCanvas.enabled)
+            {
+                _interactionCanvas.transform.LookAt(_mainCamera.transform);
+                _interactionCanvas.transform.Rotate(0, 180, 0);
+            }
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _interactionCanvas.enabled = true;
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _interactionCanvas.enabled = false;
+            }
+        }
         [Inject] private IWorldStateMachine _worldStateMachine;
+        [SerializeField] private Canvas _interactionCanvas;
+        private Camera _mainCamera;
     }
 }
