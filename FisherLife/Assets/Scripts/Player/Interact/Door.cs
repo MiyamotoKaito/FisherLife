0958751fb8dda1f78d57faeee400cd7f3a8284cb
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-06 09:23:32 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Player/Interact/Door.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PlayerModule
+{
+    public class Door : InteractObjectBase
+    {
+        public override void Interact()
+        {
+            Debug.Log("ドアを開ける");
+        }
+    }
+}
