39988a404c98c80d0ddb810c7aec4fc814b2efdb
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-06 12:08:58 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Utility/CinemachineCameraPrioriyChanger.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+namespace Utility
+{
+    public class CinemachineCameraPrioriyChanger : MonoBehaviour
+    {
+        protected const int CINEMACHINE_CAMERA_PRIORITY = 0;
+
+        [SerializeField, Tooltip("使用するシネマティックカメラ。")]
+        protected CinemachineCamera _cinemachineCamera;
+        [SerializeField, Tooltip("カメラの優先度。"), Min(2)]
+        protected int _priority = 2;
+    }
+}
