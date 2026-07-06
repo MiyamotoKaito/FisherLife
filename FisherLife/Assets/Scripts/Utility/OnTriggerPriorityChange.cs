cb47f011e3020cd54d6f08a50eb7a07d39fc778f
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-06 12:09:13 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Utility/OnTriggerPriorityChange.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+    public class OnTriggerPriorityChange : CinemachineCameraPrioriyChanger
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _cinemachineCamera.Priority = _priority;
+                _cinemachineCamera.gameObject.SetActive(true);
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _cinemachineCamera.Priority = CINEMACHINE_CAMERA_PRIORITY;
+                _cinemachineCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+}
