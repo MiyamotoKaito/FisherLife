9b7093a5a1eb5d42a8945a5bf5217ef3e6bd3818
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-06 09:23:09 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PlayerModule
+{
+    public class PlayerSpawner : MonoBehaviour
+    {
+
+        private class PlayerSpawnPoint
+        {
+            public Transform Position => _position;
+            private Transform _position;
+        }
+    }
+}
