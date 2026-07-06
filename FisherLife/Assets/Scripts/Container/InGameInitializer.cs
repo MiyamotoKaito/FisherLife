eabf1f2acf3025b129ff933dc73b0bdecfe6adc3
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-06 09:22:44 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Container/InGameInitializer.cs
@@ -0,0 +1,31 @@
+﻿using Commons;
+using UnityEngine;
+using VContainer;
+
+namespace Container
+{
+    public class InGameInitializer : MonoBehaviour
+    {
+        /// <summary>
+        ///     ワールドステートマシンを注入する。
+        /// </summary>
+        [Inject]
+        public void Inject(IWorldStateMachine worldStateMachine)
+        {
+            _worldStateMachine = worldStateMachine;
+        }
+
+        [SerializeField, Tooltip("起動時に遷移するワールド状態。")]
+        private WorldStateType _worldStateType;
+
+        private IWorldStateMachine _worldStateMachine;
+
+        /// <summary>
+        ///     起動時に指定した状態へ遷移する。
+        /// </summary>
+        private void Start()
+        {
+            _worldStateMachine.ChangeState(_worldStateType);
+        }
+    }
+}
