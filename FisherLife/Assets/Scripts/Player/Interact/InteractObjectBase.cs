3f32c2e426d392c9dd171904e6974b0cba95598a
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-06 09:23:59 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Player/Interact/InteractObjectBase.cs
@@ -0,0 +1,26 @@
+﻿using Commons;
+using UnityEngine;
+
+namespace PlayerModule
+{
+    public class InteractObjectBase : MonoBehaviour, IInteractable
+    {
+        public string DisplayDescription => _displayDescription;
+
+        public virtual void Interact()
+        {
+            throw new System.NotImplementedException("このオブジェクトはインタラクションに対応していません");
+        }   
+
+        void Start()
+        {
+            if (!TryGetComponent<Collider>(out var collider))
+            {
+                Debug.LogError($"このObjectにはColliderが必要です : {name}");
+                return;
+            }
+        }
+
+       [SerializeField] private string _displayDescription;
+    }
+}
