cadb32f1e37973678eb0fcab756e64e9190a2a9a
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 07:49:00 +0900

    [update]釣り竿SO

---
+++ FisherLife/Assets/Scripts/Utility/SaveSystem/SaveData/RodData.cs
@@ -0,0 +1,15 @@
+﻿namespace Utility
+{
+    /// <summary>
+    ///     釣り竿のセーブデータ
+    /// </summary>
+    [System.Serializable]
+    public class RodData : SaveBase
+    {
+        public string RodName;
+        public byte RodLevel;
+        public int AttackPower;
+        public float CriticalMultiplier;
+        public float CriticalRate;
+    }
+}
