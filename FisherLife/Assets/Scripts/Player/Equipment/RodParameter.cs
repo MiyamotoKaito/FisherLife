a103657c35c28c72bdd763ed69ea4ddf20a85206
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 05:20:09 +0900

    [add]釣り竿のSOクラス

---
+++ FisherLife/Assets/Scripts/Player/Equipment/RodParameter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace PlayerModule
+{
+    /// <summary>
+    /// 釣り竿のSO
+    /// </summary>
+    [CreateAssetMenu(fileName = "RodParameter", menuName = "Scriptable Objects/RodParameter")]
+    public class RodParameter : ScriptableObject
+    {
+        /// <summary> 釣り竿の名前。 </summary>
+        public string Name => _name;
+        /// <summary> 攻撃力。 </summary>
+        public int AttackPower => _attackPower;
+        /// <summary> クリティカル倍率。 </summary>
+        public float CriticalMutiplier => _criticalMultiplier;
+        /// <summary> クリティカル発生倍率。 </summary>
+        public float CriticalRate => _criticalRate;
+
+        [SerializeField, Tooltip("釣り竿の名前")]
+        private string _name;
+        [Header("パラメーター設定")]
+        [SerializeField, Tooltip("釣り竿の攻撃力")]
+        private int _attackPower;
+        [SerializeField, Tooltip("クリティカル倍率")]
+        private float _criticalMultiplier;
+        [SerializeField, Tooltip("クリティカル発生倍率")]
+        private float _criticalRate;
+    }
+}
