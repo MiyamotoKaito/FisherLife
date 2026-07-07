755eb2423a675f8006ab2fe53d9d9306808463c8
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-07 19:35:07 +0900

    [update]釣り中のカメラに移動する機能実装

--- FisherLife/Assets/Scripts/Utility/FishingPriorityChange.cs
+++ FisherLife/Assets/Scripts/Utility/FishingPriorityChange.cs
@@ -1,10 +1,29 @@
-﻿namespace Utility
+﻿using Commons;
+using R3;
+using VContainer;
+
+namespace Utility
 {
     /// <summary>
     ///     釣りが始まった時のCinemachineの優先度を設定する
     /// </summary>
     public class FishingPriorityChange : CinemachineCameraPrioriyChanger
     {
+        private void Start()
+        {
+            _worldStateMachine.CurrentStateType.Subscribe(state =>
+            {
+                if (state == WorldStateType.Fishing || state == WorldStateType.Typing)
+                {
+                    _cinemachineCamera.Priority = _priority;
+                }
+                else
+                {
+                    _cinemachineCamera.Priority = CINEMACHINE_CAMERA_PRIORITY;
+                }
+            }).RegisterTo(this.destroyCancellationToken);
+        }
 
+        [Inject] private IWorldStateMachine _worldStateMachine;
     }
 }
