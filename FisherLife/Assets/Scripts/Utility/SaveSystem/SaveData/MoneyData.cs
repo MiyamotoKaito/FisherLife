40ba47315c43b60c806b693ca061507073e8c350
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 07:44:55 +0900

    [update]Serializable付け忘れ

---
+++ FisherLife/Assets/Scripts/Utility/SaveSystem/SaveData/MoneyData.cs
@@ -0,0 +1,11 @@
+﻿namespace Utility
+{
+    /// <summary>
+    /// 所持金のセーブデータ
+    /// </summary>
+    [System.Serializable]
+    public class MoneyData : SaveBase
+    {
+        public int Money;
+    }
+}
