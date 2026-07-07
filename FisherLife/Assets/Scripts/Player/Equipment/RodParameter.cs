fc28532ae6290bf228c5ed33683883d3d5f3affc
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 05:37:38 +0900

    [update]釣り竿パラメーター実装

--- FisherLife/Assets/Scripts/Player/Equipment/RodParameter.cs
+++ FisherLife/Assets/Scripts/Player/Equipment/RodParameter.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor.MPE;
+using UnityEngine;
 
 namespace PlayerModule
 {
@@ -10,6 +11,8 @@ namespace PlayerModule
     {
         /// <summary> 釣り竿の名前。 </summary>
         public string Name => _name;
+        /// <summary> 釣り竿のレベル。 </summary>
+        public byte Level => _level;
         /// <summary> 攻撃力。 </summary>
         public int AttackPower => _attackPower;
         /// <summary> クリティカル倍率。 </summary>
@@ -19,12 +22,14 @@ namespace PlayerModule
 
         [SerializeField, Tooltip("釣り竿の名前")]
         private string _name;
+        [SerializeField, Tooltip("釣り竿のレベル")]
+        private byte _level;
         [Header("パラメーター設定")]
         [SerializeField, Tooltip("釣り竿の攻撃力")]
         private int _attackPower;
         [SerializeField, Tooltip("クリティカル倍率")]
         private float _criticalMultiplier;
-        [SerializeField, Tooltip("クリティカル発生倍率")]
+        [SerializeField, Tooltip("クリティカル発生倍率"),Range(1,100)]
         private float _criticalRate;
     }
 }
