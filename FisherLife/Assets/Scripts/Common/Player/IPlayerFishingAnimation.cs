9456148f30417b4868d4c6a213dd0b9d38302d45
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 07:48:42 +0900

    [add]インターフェース追加

---
+++ FisherLife/Assets/Scripts/Common/Player/IPlayerFishingAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Commons
+{
+    public interface IPlayerFishingAnimation
+    {
+        void Throw();
+        void Fighting();
+        void Stop();
+        void GetFish();
+    }
+}
