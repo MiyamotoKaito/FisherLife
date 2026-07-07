d6183a2a9bded93cbea03f07dea06d91756cc7ee
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-07 19:41:46 +0900

    [add]ステージシーン用のLifetimeScope

---
+++ FisherLife/Assets/Scripts/Container/StageSceneLifetimeScope.cs
@@ -0,0 +1,17 @@
+﻿using PlayerModule;
+using Utility;
+using VContainer;
+using VContainer.Unity;
+
+namespace Container
+{
+    public class StageSceneLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterComponentInHierarchy<Door>();
+            builder.RegisterComponentInHierarchy<FishingArea>();
+            builder.RegisterComponentInHierarchy<FishingPriorityChange>();
+        }
+    }
+}
