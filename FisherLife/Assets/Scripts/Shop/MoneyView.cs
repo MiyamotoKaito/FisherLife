62888fc69954d743fc963fb7a46f9491020f116e
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 20:47:43 +0900

    [update]UIに表示の実装

--- FisherLife/Assets/Scripts/Shop/MoneyView.cs
+++ FisherLife/Assets/Scripts/Shop/MoneyView.cs
@@ -1,19 +1,43 @@
+﻿using Commons;
+using DG.Tweening;
+using R3;
+using TMPro;
 using UnityEngine;
+using VContainer;
 
 namespace ShopModule
 {
     public class MoneyView : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
+        [Inject]
+        private IWorldStateMachine _worldStateMachine;
+        [SerializeField]
+        private Canvas _moneyCanvas;
+        [SerializeField]
+        private TextMeshProUGUI _text;
+        private int _currentMoney;
 
-        // Update is called once per frame
-        void Update()
+        public void SetMoney(int money)
         {
-        
+            DOVirtual.Int(_currentMoney, money, 1.0f, x =>
+            {
+                _currentMoney = x;
+                _text.text = x.ToString();
+            });
+        }
+        private void Start()
+        {
+            _worldStateMachine.CurrentStateType.Subscribe(type =>
+            {
+                if (type == WorldStateType.Moving || type == WorldStateType.Fishing)
+                {
+                    _moneyCanvas.enabled = true;
+                }
+                else
+                {
+                    _moneyCanvas.enabled = false;
+                }
+            }).RegisterTo(this.destroyCancellationToken);
         }
     }
 }
