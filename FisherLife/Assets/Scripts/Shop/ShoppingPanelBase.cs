d7fe1c1fe9f3436ce82ec85d140c715ce43e575c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-09 02:00:13 +0900

    [update]実装

--- FisherLife/Assets/Scripts/Shop/ShoppingPanelBase.cs
+++ FisherLife/Assets/Scripts/Shop/ShoppingPanelBase.cs
@@ -4,13 +4,29 @@ namespace ShopModule
 {
     public class ShoppingPanelBase : MonoBehaviour
     {
+        [SerializeField]
+        private ItemBase[] _items;
+        private int _index = 0;
+        public void Begin()
+        {
+            _index = 0;
+            _items[_index].gameObject.SetActive(true);
+        }
         public virtual void Up()
         {
+            if (_index == 0) return;
 
+            _items[_index].gameObject.SetActive(false);
+            _index--;
+            _items[_index].gameObject.SetActive(true);
         }
         public virtual void Down()
         {
+            if (_index + 1 == _items.Length) return;
 
+            _items[_index].gameObject.SetActive(false);
+            _index++;
+            _items[_index].gameObject.SetActive(true);
         }
         public virtual void Right()
         {
@@ -22,7 +38,7 @@ namespace ShopModule
         }
         public virtual void Entry()
         {
-
+            _items[_index].Trade();
         }
     }
 }
