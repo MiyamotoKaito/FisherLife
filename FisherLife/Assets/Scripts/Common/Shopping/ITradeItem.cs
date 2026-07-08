bca2e93793021cbbe14c35ef3ad5744d568dc12d
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-09 02:01:40 +0900

    [add]インターフェース追加

---
+++ FisherLife/Assets/Scripts/Common/Shopping/ITradeItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Commons
+{
+    /// <summary>
+    /// 売買リストの1項目。買う竿 / 売る魚 / 売る竿 の共通抽象。
+    /// </summary>
+    public interface ITradeItem
+    {
+        string DisplayName { get; }
+        int Price { get; }
+        bool TryTrade();
+    }
+
+}
