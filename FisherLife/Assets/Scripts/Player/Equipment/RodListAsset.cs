6c42c85309405f4444ac2a709201fc8cbbc46b62
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 20:46:13 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Player/Equipment/RodListAsset.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerModule
+{
+    /// <summary>
+    /// 釣り竿のパラメータアセットをまとめるSOクラス
+    /// </summary>
+    [CreateAssetMenu(fileName = "RodListAsset", menuName = "Scriptable Objects/RodListAsset")]
+    public class RodListAsset : ScriptableObject
+    {
+        /// <summary> 釣り竿のリスト。 </summary>
+        public IReadOnlyList<RodParameter> RodParameters => _parameters;
+
+        [SerializeField, Tooltip("釣り竿のリスト")]
+        private List<RodParameter> _parameters;
+    }
+}
