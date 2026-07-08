924d6d244371af3089fc5c2487c6086f58f7b6cf
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-09 02:01:09 +0900

    [update]実装

--- FisherLife/Assets/Scripts/Shop/Item/SellItem.cs
+++ FisherLife/Assets/Scripts/Shop/Item/SellItem.cs
@@ -1,9 +1,47 @@
+﻿using Cysharp.Threading.Tasks;
+using FishModule;
+using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
+using Utility;
 
 namespace ShopModule
 {
-    public class SellItem
+    public class SellItem : ItemBase
     {
-    
+        [SerializeField]
+        private FishParameter _fishParameter;
+        private bool _isHaveFish;
+        public override async ValueTask<bool> IsTrade()
+        {
+            IsHaveFish();
+
+            return _isHaveFish;
+        }
+        private async void IsHaveFish()
+        {
+            var data = await SaveSystem.LoadAsync<FishCountData>();
+
+            _isHaveFish = data.Fishes.Exists(fish =>
+                 fish.FishName == _fishParameter.Name && fish.Count > 0);
+        }
+        public override async UniTask Trade()
+        {
+            if (await IsTrade())
+            {
+                _moneyModel.Add(_fishParameter.SellingPrice);
+
+                var fishData = await SaveSystem.LoadAsync<FishCountData>();
+
+                var f = fishData.Fishes.FirstOrDefault(fish => fish.FishName == _fishParameter.Name);
+                f.Count--;
+            }
+        }
+
+        private void Awake()
+        {
+            _displayName = _fishParameter.Name;
+            _price = _fishParameter.SellingPrice;
+        }
     }
 }
