b802dc40f511ba51aeffa432615183ebcce4eecd
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 20:46:29 +0900

    [add]LifetimeScope作成

---
+++ FisherLife/Assets/Scripts/Container/ShoppingLifetimeScope.cs
@@ -0,0 +1,26 @@
+﻿using Commons;
+using PlayerModule;
+using ShopModule;
+using VContainer;
+using VContainer.Unity;
+
+namespace Container
+{
+    public class ShoppingLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<MoneyModel>(Lifetime.Singleton);
+            builder.RegisterComponentInHierarchy<MoneyPresenter>();
+            builder.RegisterComponentInHierarchy<MoneyView>();
+            builder.Register<IController, ShoppingController>(Lifetime.Singleton);
+            builder.Register<ShoppingState>(Lifetime.Singleton).As<IState>().AsSelf();
+
+            builder.RegisterBuildCallback(resolver =>
+            {
+                var machine = resolver.Resolve<IWorldStateMachine>();
+                machine.AddState(resolver.Resolve<ShoppingState>());
+            });
+        }
+    }
+}
