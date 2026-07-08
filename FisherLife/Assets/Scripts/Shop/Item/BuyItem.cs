bbb12c2c5de1a7a0e4e55cd387ad332a4defc65f
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 16:50:23 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Shop/Item/BuyItem.cs
@@ -0,0 +1,16 @@
+﻿using FishModule;
+using UnityEngine;
+
+namespace ShopModule
+{
+    public class BuyItem : ItemBase
+    {
+        [SerializeField]
+        private FishParameter _fishParameter;
+
+        public override void Entry()
+        {
+
+        }
+    }
+}
