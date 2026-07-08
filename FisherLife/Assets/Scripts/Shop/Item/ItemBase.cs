c965a4b405d9bff7b5eaac0ccc02bb1db709e071
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-09 02:00:46 +0900

    [update]実装

--- FisherLife/Assets/Scripts/Shop/Item/ItemBase.cs
+++ FisherLife/Assets/Scripts/Shop/Item/ItemBase.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using System.Threading.Tasks;
+using UnityEngine;
+using VContainer;
 
 namespace ShopModule
 {
     public abstract class ItemBase : MonoBehaviour
     {
-        public abstract void Entry();
+        public string DisplayName => _displayName;
+
+        public int Price => _price;
+        protected string _displayName;
+        protected int _price;
+        [Inject]
+        protected MoneyModel _moneyModel;
+        public abstract ValueTask<bool> IsTrade();
+        public abstract UniTask Trade();
     }
 }
