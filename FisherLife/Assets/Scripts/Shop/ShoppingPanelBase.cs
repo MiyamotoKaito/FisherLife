41b0ab65da3c07f46c2753690e7af0b5283de0e6
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 16:49:36 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Shop/ShoppingPanelBase.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace ShopModule
+{
+    public class ShoppingPanelBase : MonoBehaviour
+    {
+        public virtual void Up()
+        {
+
+        }
+        public virtual void Down()
+        {
+
+        }
+        public virtual void Right()
+        {
+
+        }
+        public virtual void Left()
+        {
+
+        }
+        public virtual void Entry()
+        {
+
+        }
+    }
+}
