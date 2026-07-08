9b44449e2d6d09ab8939fbb4331c8a472bf6cc39
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 20:47:08 +0900

    [update]UI関連実装

--- FisherLife/Assets/Scripts/Player/Interact/Clerk.cs
+++ FisherLife/Assets/Scripts/Player/Interact/Clerk.cs
@@ -10,8 +10,36 @@ namespace PlayerModule
         {
             _worldStateMachine.ChangeState(WorldStateType.Shopping);
         }
-
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+        }
+        private void Update()
+        {
+            if (_interactionCanvas.enabled)
+            {
+                _interactionCanvas.transform.LookAt(_mainCamera.transform);
+                _interactionCanvas.transform.Rotate(0, 180, 0);
+            }
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _interactionCanvas.enabled = true; 
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _interactionCanvas.enabled = false;
+            }
+        }
         [Inject]
         private IWorldStateMachine _worldStateMachine;
+        [SerializeField] 
+        private Canvas _interactionCanvas;
+        private Camera _mainCamera;
     }
 }
