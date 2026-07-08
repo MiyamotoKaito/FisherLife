08a81658d9cdea1d0e81f753888c943ac793f9da
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 16:50:05 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Shop/ShoppingState.cs
@@ -0,0 +1,18 @@
+﻿using Commons;
+namespace ShopModule
+{
+    public class ShoppingState : IState
+    {
+        public WorldStateType WorldState => WorldStateType.Shopping;
+
+        public void Entry()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Exit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
