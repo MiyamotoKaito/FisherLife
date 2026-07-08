297c7b88aea7397c8395525e26dd352997e22fbb
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 16:47:49 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Player/Interact/Clerk.cs
@@ -0,0 +1,17 @@
+﻿using Commons;
+using UnityEngine;
+using VContainer;
+
+namespace PlayerModule
+{
+    public class Clerk : InteractObjectBase
+    {
+        public override void Interact()
+        {
+            _worldStateMachine.ChangeState(WorldStateType.Shopping);
+        }
+
+        [Inject]
+        private IWorldStateMachine _worldStateMachine;
+    }
+}
