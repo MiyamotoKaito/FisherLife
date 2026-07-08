36cdffe3eb2727ca16d13988d64f98a07205d538
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 12:34:17 +0900

    [update]クラス追加

---
+++ FisherLife/Assets/Scripts/Battle/BattleResultView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace BattleModule
+{
+    public class BattleResultView : MonoBehaviour
+    {
+        public void SetEnable(bool enable)
+        {
+            _resultCanvas.enabled = enable;
+        }
+
+        [SerializeField]
+        private Canvas _resultCanvas;
+    }
+}
