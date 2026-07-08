26b0867290996c90882f658e66627e74729e563a
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 20:48:18 +0900

    [update]SaveBaseの継承

--- FisherLife/Assets/Scripts/Utility/SaveSystem/SaveData/FishCountData.cs
+++ FisherLife/Assets/Scripts/Utility/SaveSystem/SaveData/FishCountData.cs
@@ -6,7 +6,7 @@ namespace Utility
     /// 所持している魚の数(種類ごと)のセーブデータ
     /// </summary>
     [System.Serializable]
-    public class FishCountData
+    public class FishCountData : SaveBase
     {
         // Dictionaryの保存は6.5に移行してから
         public List<FishCountEntry> Fishes = new();
