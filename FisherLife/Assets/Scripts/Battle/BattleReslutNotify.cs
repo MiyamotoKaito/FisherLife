d4c812012b844ce1277269a98cd35b40fdc7cee7
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 12:34:00 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Battle/BattleReslutNotify.cs
@@ -0,0 +1,18 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace BattleModule
+{
+    public class BattleReslutNotify
+    {
+        public BattleReslutNotify(BattleResultView battleResultView)
+        {
+            _battleResultView = battleResultView;
+        }
+
+        public async UniTask NotifyResult()
+        {
+            
+        }
+        private readonly BattleResultView _battleResultView;
+    }
+}
