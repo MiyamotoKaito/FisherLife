57ce6e102097a33eac51e19d8304b36c907a858e
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-08 13:10:14 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Utility/SaveSystem/SaveData/FishCountData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Utility
+{
+    /// <summary>
+    /// 所持している魚の数(種類ごと)のセーブデータ
+    /// </summary>
+    public class FishCountData
+    {
+        // Dictionaryの保存は6.5に移行してから
+        public List<FishCountEntry> Fishes = new();
+    }
+}
