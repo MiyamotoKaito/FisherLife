2841470b7bf2442f9814c6bb80c600a9edd04b6c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-09 02:01:53 +0900

    [add]クラス追加

---
+++ FisherLife/Assets/Scripts/Shop/Item/NextPanelItem.cs
@@ -0,0 +1,17 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace ShopModule
+{
+    public class NextPanelItem : ItemBase
+    {
+        public override UniTask<bool> IsTrade()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override UniTask Trade()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
