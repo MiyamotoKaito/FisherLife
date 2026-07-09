706d6c67e6b68e9c805fe0e0993a16b006085c66
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-09 16:56:25 +0900

    [update]ステートの実装

--- FisherLife/Assets/Scripts/Shop/ShoppingState.cs
+++ FisherLife/Assets/Scripts/Shop/ShoppingState.cs
@@ -1,18 +1,33 @@
-﻿using Commons;
+using Commons;
+
 namespace ShopModule
 {
+    /// <summary>
+    ///     買い物中のワールド状態。アクションマップを積んでショップ操作を開始する。
+    /// </summary>
     public class ShoppingState : IState
     {
+        public ShoppingState(IInputActionMapStack inputActionMapStack, ShoppingController shoppingController)
+        {
+            _inputActionMapStack = inputActionMapStack;
+            _shoppingController = shoppingController;
+        }
+
         public WorldStateType WorldState => WorldStateType.Shopping;
 
         public void Entry()
         {
-            throw new System.NotImplementedException();
+            _inputActionMapStack.Push(InputActionMapType.Shopping);
+            _shoppingController.Begin();
         }
 
         public void Exit()
         {
-            throw new System.NotImplementedException();
+            _shoppingController.End();
+            _inputActionMapStack.Pop();
         }
+
+        private readonly IInputActionMapStack _inputActionMapStack;
+        private readonly ShoppingController _shoppingController;
     }
 }
