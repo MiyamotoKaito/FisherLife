a191324055cce6a672431b95740db657c80d9982
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-09 17:01:36 +0900

    [update]インベントリを開く際のシステム実装

--- FisherLife/Assets/Scripts/Player/PlayerController.cs
+++ FisherLife/Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using Commons;
+using Commons;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,18 +16,21 @@ namespace PlayerModule
             PlayerMovePresenter playerMovePresenter,
             Camera camera,
             PlayerInteractor playerInteractor,
-            IPlayerMoveUsecase playerMoveUsecase)
+            IPlayerMoveUsecase playerMoveUsecase,
+            IWorldStateMachine worldStateMachine)
         {
             _inputActions = inputActions;
             _playerMovePresenter = playerMovePresenter;
             _camera = camera;
             _playerMoveUsecase = playerMoveUsecase;
             _playerInteractor = playerInteractor;
+            _worldStateMachine = worldStateMachine;
 
             // 同名プロパティ（InputActionMapType.Player）の文字列でマップを取得する。
             _playerMap = _inputActions.FindActionMap(InputActionMapType.ToString(), true);
             _moveAction = _playerMap.FindAction(MOVE_ACTION);
             _interactAction = _playerMap.FindAction(INTERACT_ACTION);
+            _equipmentAction = _playerMap.FindAction(EQUIPMENT_ACTION); // 未定義なら null
         }
 
         /// <summary> 対応するアクションマップ種別。 </summary>
@@ -41,6 +44,7 @@ namespace PlayerModule
             _moveAction.performed += MoveHandler;
             _moveAction.canceled += MoveHandler;
             _interactAction.started += InteractHandler;
+            if (_equipmentAction != null) _equipmentAction.started += OpenEquipmentHandler;
         }
 
         /// <summary>
@@ -51,6 +55,7 @@ namespace PlayerModule
             _moveAction.performed -= MoveHandler;
             _moveAction.canceled -= MoveHandler;
             _interactAction.started -= InteractHandler;
+            if (_equipmentAction != null) _equipmentAction.started -= OpenEquipmentHandler;
         }
 
         /// <summary>
@@ -63,15 +68,18 @@ namespace PlayerModule
 
         private const string MOVE_ACTION = "Move";
         private const string INTERACT_ACTION = "Interact";
+        private const string EQUIPMENT_ACTION = "Equipment";
 
         private readonly InputActionAsset _inputActions;
         private readonly InputActionMap _playerMap;
         private readonly InputAction _moveAction;
         private readonly InputAction _interactAction;
+        private readonly InputAction _equipmentAction;
         private readonly PlayerMovePresenter _playerMovePresenter;
         private readonly IPlayerMoveUsecase _playerMoveUsecase;
         private readonly Camera _camera;
         private readonly PlayerInteractor _playerInteractor;
+        private readonly IWorldStateMachine _worldStateMachine;
 
         /// <summary>
         ///     移動入力を受け取り、変換した方向をPresenterへ渡す。
@@ -91,6 +99,11 @@ namespace PlayerModule
             {
                 _playerInteractor.Interact();
             }
+        }
+
+        private void OpenEquipmentHandler(InputAction.CallbackContext callbackContext)
+        {
+            _worldStateMachine.ChangeState(WorldStateType.Equipment);
         }
     }
 }
