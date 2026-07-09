928ac354a8a013dfedc78916ed8dd0f60f33018f
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-09 16:47:26 +0900

    [add]セーブデータクラス追加

---
+++ FisherLife/Assets/Scripts/Utility/SaveSystem/SaveData/RodCountData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Utility
+{
+    public class RodCountData : SaveBase
+    {
+        public List<RodData> RodList = new();
+        /// <summary> 装備中の竿の名前。 </summary>
+        public string EquippedRodName;
+    }
+}
