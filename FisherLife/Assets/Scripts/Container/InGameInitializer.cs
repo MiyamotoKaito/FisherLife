f7e9bec77b47114ebd573176aca7c89818436bb5
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-09 16:49:27 +0900

    [update]インベントリ用の実装追加

--- FisherLife/Assets/Scripts/Container/InGameInitializer.cs
+++ FisherLife/Assets/Scripts/Container/InGameInitializer.cs
@@ -1,4 +1,6 @@
-﻿using Commons;
+using Commons;
+using Cysharp.Threading.Tasks;
+using PlayerModule;
 using Unity.Cinemachine;
 using UnityEngine;
 using VContainer;
@@ -8,26 +10,35 @@ namespace Container
     public class InGameInitializer : MonoBehaviour
     {
         /// <summary>
-        ///     ワールドステートマシンを注入する。
+        ///     ワールドステートマシンと竿インベントリを注入する。
         /// </summary>
         [Inject]
-        public void Inject(IWorldStateMachine worldStateMachine)
+        public void Inject(IWorldStateMachine worldStateMachine, RodInventoryModel rodInventoryModel)
         {
             _worldStateMachine = worldStateMachine;
+            _rodInventoryModel = rodInventoryModel;
         }
 
         private const int CINEMACHINE_CAMERA_PRIORITY = 0;
         [SerializeField, Tooltip("起動時に遷移するワールド状態。")]
         private WorldStateType _worldStateType;
         [SerializeField, Tooltip("初期化時に使わないシネマティックカメラの配列。")]
-        private CinemachineCamera[] _cameras; 
+        private CinemachineCamera[] _cameras;
         private IWorldStateMachine _worldStateMachine;
+        private RodInventoryModel _rodInventoryModel;
 
         /// <summary>
-        ///     起動時に指定した状態へ遷移する。
+        ///     竿の装備を確定させてから、指定した状態へ遷移する。
         /// </summary>
         private void Start()
         {
+            InitializeAsync().Forget();
+        }
+
+        private async UniTaskVoid InitializeAsync()
+        {
+            await _rodInventoryModel.InitializeAsync();
+
             _worldStateMachine.ChangeState(_worldStateType);
             foreach (var cam in _cameras)
             {
