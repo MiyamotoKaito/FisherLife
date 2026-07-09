07ee41fe5fbea086cb44b7b9f89643bd6c65ae4a
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-09 16:55:37 +0900

    [update]選択されているときのアニメーションを制御する機能

--- FisherLife/Assets/Scripts/Shop/Item/ItemBase.cs
+++ FisherLife/Assets/Scripts/Shop/Item/ItemBase.cs
@@ -1,20 +1,60 @@
-﻿using Cysharp.Threading.Tasks;
-using System.Threading.Tasks;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
-using VContainer;
 
 namespace ShopModule
 {
+    /// <summary>
+    ///     ショップの1行の基底。選択(Entry)された時の振る舞いだけを共通に持つ。
+    /// </summary>
     public abstract class ItemBase : MonoBehaviour
     {
         public string DisplayName => _displayName;
-
         public int Price => _price;
+        public Sprite Sprite => _sprite;
         protected string _displayName;
         protected int _price;
-        [Inject]
-        protected MoneyModel _moneyModel;
-        public abstract ValueTask<bool> IsTrade();
-        public abstract UniTask Trade();
+        protected Sprite _sprite;
+
+        [SerializeField, Tooltip("DisplayNameを表示するテキスト。")]
+        private TextMeshProUGUI _displayText;
+        [SerializeField, Tooltip("選択中のスケール倍率。")]
+        private float _selectedScale = 1.2f;
+        [SerializeField, Tooltip("スケール変化の時間(秒)。")]
+        private float _scaleDuration = 0.15f;
+
+        private Vector3 _baseScale;
+        private bool _scaleCaptured;
+
+        /// <summary> この行が選択(Entry)された時の遷移。 </summary>
+        public abstract void OnEntry(ShoppingController controller);
+
+        /// <summary> カーソルが乗っている行だけ少し拡大する。 </summary>
+        public void SetSelected(bool selected)
+        {
+            if (!_scaleCaptured)
+            {
+                _baseScale = transform.localScale;
+                _scaleCaptured = true;
+            }
+
+            transform.DOKill();
+            transform.DOScale(selected ? _baseScale * _selectedScale : _baseScale, _scaleDuration);
+        }
+
+        /// <summary> _displayName をテキストへ反映する（再バインド時にも呼ぶ）。 </summary>
+        protected void ApplyDisplayName()
+        {
+            if (_displayText != null)
+            {
+                _displayText.text = _displayName;
+            }
+        }
+
+        // 各サブクラスの Awake で _displayName を設定した後(Start)に反映する。
+        protected virtual void Start()
+        {
+            ApplyDisplayName();
+        }
     }
 }
