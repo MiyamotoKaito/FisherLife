1b1b7ad8eb4bf2132a4a6a7bdedc24fb419e0368
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-07-21 21:37:26 +0900

    [update]サマリ付け

--- FisherLife/Assets/Scripts/Battle/TimeLimitView.cs
+++ FisherLife/Assets/Scripts/Battle/TimeLimitView.cs
@@ -1,13 +1,19 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace BattleModule
 {
+    /// <summary>
+    /// 時間制限のView
+    /// </summary>
     public class TimeLimitView : MonoBehaviour
     {
         [SerializeField]
         private Image _timeLimitImage;
-
+        /// <summary>
+        /// 残り時間を画像のFillAmountに適応する
+        /// </summary>
+        /// <param name="amount"></param>
         public void UpdateImage(float amount)
         {
             if (_timeLimitImage != null)
